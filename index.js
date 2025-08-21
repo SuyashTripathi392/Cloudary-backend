2c974b3779ac9e64156f538ba1fea62ac96e7e53
Suyash Tripathi <rajtr3142@gmail.com>
2025-08-21 14:00:41 +0530

    rollback  origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 
 
 app.use(cors({
-  origin: "https://cloudary-frontend.vercel.app/", 
+  origin: "https://cloudary-frontend.vercel.app", 
   credentials: true
 }));
 
